b0f53856d5d65ee71c9b61ce2d267e4959341367
Vương Hiếu <hieu.vuong1122@gmail.com>
2020-02-09 12:18:08 +0700

    SmartGit: Create Main Class JAVA.

---
+++ 01-hello-world/src/com/hieu/app/Main.java
@@ -0,0 +1,20 @@
+/**
+ * SAYID
+ * 2020
+ */
+package com.hieu.app;
+
+/**
+ * @author SAYID
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
